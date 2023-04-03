6788c9122acd3e252f71ec29ab5d8e694bd9760a
Sayat <129245004+qwufy@users.noreply.github.com>
2023-04-03 16:15:11 +0600

    hh

--- src/Problem2.java
+++ src/Problem2.java
@@ -1,5 +1,21 @@
 public class Problem2 {
     public static void main(String[] args) {
+        int n = 4;
+        int[] arr = {3,2,4,1};
+        double avg = calculateAvg(n, arr);
+        System.out.println(avg);
+    }
 
+    public static double calculateAvg(int n, int[] arr){
+        return calculateSum(n, arr, 0) / (double) n;
+    }
+
+    public static int calculateSum(int n, int[] arr, int index){
+        if (index == n) {
+            return 0;
+        }
+        else {
+            return arr[index] + calculateSum(n, arr, index+1);
+        }
     }
 }
