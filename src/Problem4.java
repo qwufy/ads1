2afc8c901eae13475a68677428e0e311978ea481
Sayat <129245004+qwufy@users.noreply.github.com>
2023-04-03 16:38:55 +0600

    hh

--- src/Problem4.java
+++ src/Problem4.java
@@ -1,2 +1,16 @@
 public class Problem4 {
+    public static void main(String[] args) {
+        int n =5;
+        int nfactorial = factorial(n);
+        System.out.println(nfactorial);
+    }
+
+    public static int factorial(int n){
+        if (n == 0 || n == 1){
+            return 1;
+        }
+        else{
+            return n*factorial(n-1);
+        }
+    }
 }
